4fe2f5865373fcd6a4fdc2ce5009156c75b30ae9
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-09-17 10:00:34 +0700

    Time: 1 ms (57.45%) | Memory: 57.8 MB (57.49%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let pos = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i-1]) {
+            nums[pos] = nums[i];
+            pos+=1
+        }
+    }
+    return pos;
+
+};
